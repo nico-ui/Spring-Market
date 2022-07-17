a0c169ec2d9653949da2586ba7cfb0688ad149a8
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-07-16 19:25:15 -0500

    Ejemplo Hola Mundo

---
+++ src/test/java/com/platzi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
