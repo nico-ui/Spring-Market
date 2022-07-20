1a11ecde94840a691a1651688cf19faf54be7e8c
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-07-19 20:51:28 -0500

    Buenas practicas para imports de clase Producto y Compra

--- src/main/java/com/platzi/demo/persistence/entity/Producto.java
+++ src/main/java/com/platzi/demo/persistence/entity/Producto.java
@@ -1,7 +1,11 @@
 package com.platzi.demo.persistence.entity;
 
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 @Entity
 @Table(name = "productos")//nombre de clase diff a nombre de tabla en DB
 public class Producto {
