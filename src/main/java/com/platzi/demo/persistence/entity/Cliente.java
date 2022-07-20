d7009abf195ffa55ef54acb903403722e96feade
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-07-20 12:28:07 -0500

    Implementando lombok en entity Cliente

--- src/main/java/com/platzi/demo/persistence/entity/Cliente.java
+++ src/main/java/com/platzi/demo/persistence/entity/Cliente.java
@@ -1,5 +1,8 @@
 package com.platzi.demo.persistence.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,64 +14,22 @@ import javax.persistence.Table;
 public class Cliente {
 
     @Id
+    @Getter @Setter
     private String id;
 
+    @Getter @Setter
     private String nombre;
 
+    @Getter @Setter
     private String apellidos;
 
+    @Getter @Setter
     private Integer celular;
 
+    @Getter @Setter
     private String direccion;
 
-    @Column(name = "correo_electronico")
+    @Getter @Setter @Column(name = "correo_electronico")
     private String correoElectronico;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public Integer getCelular() {
-        return celular;
-    }
-
-    public void setCelular(Integer celular) {
-        this.celular = celular;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getCorreoElectronico() {
-        return correoElectronico;
-    }
-
-    public void setCorreoElectronico(String correoElectronico) {
-        this.correoElectronico = correoElectronico;
-    }
 }
