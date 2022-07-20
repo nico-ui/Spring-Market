65ab566c96bfc5a6422308dc3efb459b24357c69
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-07-19 20:35:28 -0500

    Clases Compra, Producto mapeada a tablas

---
+++ src/main/java/com/platzi/demo/persistence/entity/Producto.java
@@ -0,0 +1,85 @@
+package com.platzi.demo.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")//nombre de clase diff a nombre de tabla en DB
+public class Producto {
+
+    @Id
+    @Column(name = "id_producto")//asociar nombre column con var
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Autoincrementable
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;//Buena practica notación camello
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    private boolean estado;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
