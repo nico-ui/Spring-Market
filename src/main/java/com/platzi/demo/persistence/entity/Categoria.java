ce1dc9eec81db4d1222c00a3563eabb58b042264
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-07-20 12:37:09 -0500

    Relacion Categoria Producto

--- src/main/java/com/platzi/demo/persistence/entity/Categoria.java
+++ src/main/java/com/platzi/demo/persistence/entity/Categoria.java
@@ -2,13 +2,14 @@ package com.platzi.demo.persistence.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "categorias")
@@ -24,4 +25,7 @@ public class Categoria {
 
     @Getter @Setter
     private boolean estado;
+
+    @Getter @Setter @OneToMany(mappedBy = "categoria")//Relacion que respalda
+    private List<Producto> productos;
 }
--- src/main/java/com/platzi/demo/persistence/entity/Producto.java
+++ src/main/java/com/platzi/demo/persistence/entity/Producto.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 @Entity
 @Table(name = "productos")//nombre de clase diff a nombre de tabla en DB
@@ -35,5 +37,9 @@ public class Producto {
 
     @Getter @Setter
     private boolean estado;
+
+    @ManyToOne
+    @Getter @Setter @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    private Categoria categoria;//recuperar categoria pertenece a un producto
 
 }
