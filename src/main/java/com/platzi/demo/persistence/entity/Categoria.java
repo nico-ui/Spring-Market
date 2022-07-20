ebd3a2abc167c30e3d8a20249b28d1a9595fc5a1
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-07-19 20:50:18 -0500

    Clases Categoria, Cliente mapeada a tablas

---
+++ src/main/java/com/platzi/demo/persistence/entity/Categoria.java
@@ -0,0 +1,46 @@
+package com.platzi.demo.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @Column(name = "id_categoria")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCategoria;
+
+    private String descripcion;
+
+    private boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
