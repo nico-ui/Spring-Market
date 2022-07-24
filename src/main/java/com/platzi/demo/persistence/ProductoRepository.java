18dd63ce492afb4d9dec5f3be9a60cef574a6a7e
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-07-23 22:19:15 -0500

    Primer Repository de la app

--- src/main/java/com/platzi/demo/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/demo/persistence/ProductoRepository.java
@@ -2,11 +2,15 @@ package com.platzi.demo.persistence;
 
 import com.platzi.demo.persistence.crud.ProductoCrudRepository;
 import com.platzi.demo.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
+    //Clase encargada de interactuar con la DB,
+    //@Repository es un estereotipo de spring framework
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){//Recuperar todos los productos de la DB
@@ -20,4 +24,20 @@ public class ProductoRepository {
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
+
+    //consultar pr id
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    //save
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    //borrar por id
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
 }
