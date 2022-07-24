ffa7899b14d16facb9eaa0094ef906f7ed79a4a1
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-07-23 21:58:23 -0500

    Query Methods

--- src/main/java/com/platzi/demo/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/demo/persistence/ProductoRepository.java
@@ -4,11 +4,20 @@ import com.platzi.demo.persistence.crud.ProductoCrudRepository;
 import com.platzi.demo.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){//Recuperar todos los productos de la DB
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNameASC(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByIdCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/platzi/demo/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/demo/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,14 @@ package com.platzi.demo.persistence.crud;
 import com.platzi.demo.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
     //Tabla y tipo de llave primaria
+
+    //QueryMethods:
+    List<Producto> findByIdCategoriaOrderByNameASC(int idCategoria);
+
+    Optional<List<Producto>> findByIdCantidadStockLessThanAndEstado(int CantidadStock, boolean estado);
 }
